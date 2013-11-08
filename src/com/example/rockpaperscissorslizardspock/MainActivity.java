7edb4be6268dcd5eb23fc0f18db4b716a65a39fa
Kristen Thompson <kmth225@cs.uky.edu>
2013-11-07 21:38:22 -0500

    new main activity

--- src/com/example/rockpaperscissorslizardspock/MainActivity.java
+++ src/com/example/rockpaperscissorslizardspock/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.rockpaperscissorslizardspock;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final Button joinGame= (Button)findViewById(R.id.joinGame);
+		joinGame.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				
+			Intent intent = new Intent(MainActivity.this, JoinGame.class);
+			EditText username= (EditText)findViewById(R.id.username);
+			String message = username.getText().toString();
+			
+			intent.putExtra("name",message);
+			startActivity(intent); 
+			
+			}
+		});
 	}
 
 	@Override
